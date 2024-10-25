9712e7987bff06f2c9810ad89de83490571a104d
Kavit Gami <kavit.gami@gmail.com>
2024-10-25 11:44:27 -0600

    Added files

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+
+if(process.env.NODE_ENV != "production" ){
+  require('dotenv').config();
+}
+
+
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -6,6 +13,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
@@ -14,12 +22,13 @@ const passport = require("passport");
 const localStrategy = require("passport-local");
 const User = require("./models/user.js");
 
+const dbUrl = process.env.ATLASDB_URL;
 main().then(()=>{
     console.log("Connected to DB");
 }).catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/AirBnb');
+  await mongoose.connect(dbUrl);
 
 }
 
@@ -30,8 +39,20 @@ app.use(methodOverride("_method"))
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "public")))
 
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret:process.env.SECRET,
+  },
+  touchAfter: 24*3600,
+});
+
+store.on("error", () =>{
+  console.log("ERROR IN MONGO SESSION STORE",err);
+})
 const sessionOptions = {
-  secret: "mysupersecretcode",
+  store,
+  secret:process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie:{
@@ -39,6 +60,8 @@ const sessionOptions = {
     maxAge: 7 * 24*60* 60*1000,
   }
 };
+
+
 app.use(session(sessionOptions));
 app.use(flash());
 
@@ -67,9 +90,9 @@ let registeredUser = await User.register(fakeUser, "helloworld");
 res.send(registeredUser);
 })*/
 
-app.get("/", (req, res)=>{
-    res.send("Hi, I am root")
-})
+//app.get("/", (req, res)=>{
+ //   res.send("Hi, I am root")
+//})
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
--- routes/review.js
+++ routes/review.js
@@ -6,26 +6,12 @@ const Review = require("../models/review.js");
 const Listing = require("../models/listing.js")
 const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
 
+const reviewController  = require("../controllers/reviews.js")
 //Reviews
 //Post Review Route
-router.post("/", validateReview, isLoggedIn, wrapAsync(async(req, res) =>{
-    let listing = await Listing.findById(req.params.id);
-    let newReview =new Review(req.body.review);
-    newReview.author = req.user._id;
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-    req.flash("success", "New Review Created")
-    res.redirect(`/listings/${listing._id}`);
-  }));
+router.post("/", validateReview, isLoggedIn, wrapAsync(reviewController.createReview));
   
   //Delete review Route
-router.delete("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-    await Review.findByIdAndDelete(reviewId);
-    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    req.flash("success", "Review Deleted")
-    res.redirect(`/listings/${id}`);
-  }));
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync(reviewController.destroyReview));
 
 module.exports = router;
